88b12aea65f84102e4ff3f68b70eee4a51cf56cf
HoshimovAdham <qorablack3@gmail.com>
2025-01-21 20:30:58 +0500

    uyga vazifa

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def menu() -> None:
     print("\nMenu:")
     print("\t1. Register User\n\t2. Filter users by age\n\t3. Filter users by job.\n\t4. Get all users")
 
-def print_users(users: list[dict]) -> list[dict]:
+def print_users(users: list[dict]) -> None:
     print("\nA List of Users:")
     for number, user in enumerate(users, start=1):
         print(f"{number}. {user['name']} is {user['age']} and {user['job']}.")
@@ -21,11 +21,13 @@ def add_user(users: list[dict]) -> list[dict]:
     users.append(new_user)
     return users
 
-def get_users_by_age(users: list[dict], age: int) -> list[dict]:
-    pass
+def get_users_by_age(users: list[dict], in_age: int) -> list[dict]:
+    filtered_users_age = list(filter(lambda user: user['age'] >= in_age, users))
+    return filtered_users_age
 
 def get_users_by_job(users: list[dict], job: str) -> list[dict]:
-    pass
+    filtered_users_job = list(filter(lambda user: user['job'].lower() == job.lower(), users))
+    return filtered_users_job
 
 def main():
     users = [
@@ -49,10 +51,12 @@ def main():
             users = add_user(users)
         elif choice == '2':
             age = int(input("Enter min age: "))
-            filtered_users = get_users_by_age(users, age)
+            filtered_users_age = get_users_by_age(users, age)
+            print_users(filtered_users_age)
         elif choice == '3':
-            job = int(input("Enter job: "))
-            filtered_users = get_users_by_job(users, job)
+            job = (input("Enter job: "))
+            filtered_users_job = get_users_by_job(users, job)
+            print_users(filtered_users_job)
         elif choice == '4':
             print_users(users)
         else:
